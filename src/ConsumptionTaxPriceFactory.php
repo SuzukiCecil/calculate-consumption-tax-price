9acd6d1e46a58555ef05753d92d6db33d5cee450
鈴木 聖史 <d210009@dc0397-mac.local>
2022-09-04 15:39:41 +0900

    Fix ConsumptionTaxPriceFactory For All ConsumptionTax

--- src/ConsumptionTaxPriceFactory.php
+++ src/ConsumptionTaxPriceFactory.php
@@ -5,10 +5,20 @@ namespace SuzukiCecil\CalculateConsumptionTaxPrice;
 use DateTimeImmutable;
 use InvalidArgumentException;
 use SuzukiCecil\CalculateConsumptionTaxPrice\ValueObject\FirstConsumptionTax;
+use SuzukiCecil\CalculateConsumptionTaxPrice\ValueObject\ForthConsumptionTax;
 use SuzukiCecil\CalculateConsumptionTaxPrice\ValueObject\Price;
+use SuzukiCecil\CalculateConsumptionTaxPrice\ValueObject\SecondConsumptionTax;
+use SuzukiCecil\CalculateConsumptionTaxPrice\ValueObject\ThirdConsumptionTax;
+use Exception;
 
 class ConsumptionTaxPriceFactory
 {
+    /**
+     * @param int $priceWithoutConsumptionTax
+     * @param DateTimeImmutable $date
+     * @return Price
+     * @throws Exception
+     */
     public static function create(
         int $priceWithoutConsumptionTax,
         DateTimeImmutable $date = new DateTimeImmutable(),
@@ -16,6 +26,13 @@ class ConsumptionTaxPriceFactory
         if ($priceWithoutConsumptionTax < 0) {
             throw new InvalidArgumentException("Minus price given.");
         }
-        return new Price($priceWithoutConsumptionTax, new FirstConsumptionTax());
+        $consumptionTax = match (true) {
+            FirstConsumptionTax::canApply($date) => new FirstConsumptionTax(),
+            SecondConsumptionTax::canApply($date) => new SecondConsumptionTax(),
+            ThirdConsumptionTax::canApply($date) => new ThirdConsumptionTax(),
+            ForthConsumptionTax::canApply($date) => new ForthConsumptionTax(),
+            default => throw new Exception("Consumption tax can`t apply."),
+        };
+        return new Price($priceWithoutConsumptionTax, $consumptionTax);
     }
 }
